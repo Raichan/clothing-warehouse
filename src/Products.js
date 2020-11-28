a3f0775c1036638ebc109536bc97d4d5fcef4367
Laura Sirola <laura.m.sirola@gmail.com>
2020-11-29 01:34:14 +0200

    Simplify availabilities

--- src/Products.js
+++ src/Products.js
@@ -9,6 +9,7 @@ const Products = ({ category, productList, updateProductList }) => {
   const [search, setSearch] = useState(""); // Search bar
   const [searchResults, setSearchResults] = useState(productList); // Filters results
   const [loadingError, setLoadingError] = useState(false); // Availability API error flag
+  const [availability, setAvailability] = useState({});
   const productUrl = "https://bad-api-assignment.reaktor.com/products/";
   const availabilityUrl =
     "https://bad-api-assignment.reaktor.com/availability/";
@@ -26,89 +27,67 @@ const Products = ({ category, productList, updateProductList }) => {
     setSearchResults(results);
   }, [search, productList]);
 
+  // Add manufacturer to availability info
+  const addAvailabilities = (availabilities) => {
+    let newList = { ...availability, ...availabilities };
+    setAvailability(newList);
+  };
+
   // Load products and their availability
   const getProducts = () => {
     setLoadingError(false); // Reset loading status
-    axios.get(productUrl + category).then((res) => {
-      const productData = res.data;
+    axios
+      .get(productUrl + category)
+      .then((res) => {
+        const productData = res.data;
 
-      // Find all manufacturers
-      let manufacturers = [];
-      productData.forEach((product) => {
-        if (!manufacturers.includes(product.manufacturer)) {
-          manufacturers.push(product.manufacturer);
-        }
-      });
+        // Find all manufacturers
+        let manufacturers = [];
+        productData.forEach((product) => {
+          if (!manufacturers.includes(product.manufacturer)) {
+            manufacturers.push(product.manufacturer);
+          }
+        });
 
-      // Set up call and availability object for each manufacturer
-      let axiosCalls = [];
-      let availabilities = [];
-      manufacturers.forEach((manufacturer, i) => {
-        axiosCalls.push(axios.get(availabilityUrl + manufacturer));
-        availabilities.push({ name: manufacturer, list: [] });
-      });
+        updateProductList(category, productData);
 
-      axios.all(axiosCalls).then(
-        axios.spread((...responses) => {
-          responses.forEach((res, i) => {
-            let result = res.data.response;
-            // Check that response is longer than [] (API error)
-            if (result.length > 2) {
-              availabilities[i].list = result;
-            } else {
-              // API error, set error flag
-              console.log("Availability API error: " + availabilities[i].name);
-              setLoadingError(true);
-            }
-          });
-
-          // Loop through products, set availabilities
-          let productsUpdate = [];
-          const parser = new DOMParser();
-          availabilities.forEach((manufacturer) => {
-            let manufacturerProducts = productData.filter(
-              (product) => product.manufacturer === manufacturer.name
-            );
-
-            // Skip failed manufacturers
-            if (manufacturer.list.length > 0) {
-              console.log("handling " + manufacturer.name);
-              manufacturerProducts.forEach((product) => {
-                // Find availability data matching product id
-                let avLine = manufacturer.list.find(
-                  (p) => p.id.toLowerCase() === product.id
-                );
-                if (avLine !== undefined) {
-                  // Parse availability status
+        // getAvailabilities(manufacturers);
+        // Set up call and availability object for each manufacturer
+        let availabilities = {};
+        const parser = new DOMParser();
+        manufacturers.forEach((manufacturer, i) => {
+          axios
+            .get(availabilityUrl + manufacturer)
+            .then((res) => {
+              console.log("handling " + manufacturer);
+              let result = res.data.response;
+              // Check that response is longer than [] (API error)
+              if (result.length > 2) {
+                result.forEach((a) => {
                   const availabilityXml = parser.parseFromString(
-                    avLine.DATAPAYLOAD,
+                    a.DATAPAYLOAD,
                     "application/xml"
                   );
-                  // Update product availability
                   let availability = availabilityXml.getElementsByTagName(
                     "INSTOCKVALUE"
                   )[0].childNodes[0].nodeValue;
-                  product.availability = availability;
-                  productsUpdate.push(product);
-                }
-              });
-            } else {
-              console.log("skipped " + manufacturer.name);
-              // Show failed products in the list anyway
-              manufacturerProducts.forEach((product) => {
-                productsUpdate.push(product);
-              });
-            }
-          });
-
-          // All products have been handled, sort alphabetically and update availabilities
-          productsUpdate = productsUpdate.sort((a, b) =>
-            a.name > b.name ? 1 : -1
-          );
-          updateProductList(category, productsUpdate);
-        })
-      );
-    });
+                  availabilities[a.id.toLowerCase()] = availability;
+                });
+                addAvailabilities(availabilities);
+              } else {
+                // API error, set error flag
+                console.error("Availability API error: " + manufacturer);
+                setLoadingError(true);
+              }
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   // Fetch products on page load
@@ -161,6 +140,7 @@ const Products = ({ category, productList, updateProductList }) => {
           Reload
         </button>
       </p>
+      {/* TODO .sort((a, b) => a.name > b.name ? 1 : -1); */}
       <div className="productTable">
         <Table striped>
           <tbody>
@@ -179,7 +159,7 @@ const Products = ({ category, productList, updateProductList }) => {
                     {/* Took the liberty of assuming these are euros*/}
                   </td>
                   <td>
-                    <Availability availability={item.availability} />
+                    <Availability availability={availability[item.id]} />
                   </td>
                 </tr>
               );
